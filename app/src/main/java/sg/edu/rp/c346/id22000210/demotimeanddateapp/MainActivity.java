f6a089cfd0fc5c1b87c130a7452bdde2622334b7
Wayne <22000210@myrp.edu.sg>
2023-05-11 11:34:33 +0800

    completed

--- app/src/main/java/sg/edu/rp/c346/id22000210/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000210/demotimeanddateapp/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnDisplayDate;
     Button btnDisplayTime;
     TextView tvDisplay;
+    Button btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +30,40 @@ public class MainActivity extends AppCompatActivity {
         btnDisplayDate = findViewById(R.id.buttonDisplayDate);
         btnDisplayTime = findViewById(R.id.buttonDisplayTime);
         tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.reset);
 
         btnDisplayTime.setOnClickListener(new View.OnClickListener() {
             //It ties a listener object when the button is clicked, and it will then activate the method (action)
             @Override
             public void onClick(View v) {
                 //Code for the action of Display Time button
-                btnDisplayTime =
+                tvDisplay.setText("Time is " + tp.getHour() + ":" + tp.getMinute());
 
 
             }
         });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Code for action to handle Display Date button click event
+                tvDisplay.setText("Date is " + dp.getDayOfMonth() + "/" + (dp.getMonth() + 1) + "/" + dp.getYear());
+
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Action to handle Reset button click event
+                tvDisplay.setText("");
+                dp.updateDate(2020, 0, 1);
+                tp.setHour(0);
+                tp.setMinute(0);
+            }
+        });
+
+
 
     }
 }
