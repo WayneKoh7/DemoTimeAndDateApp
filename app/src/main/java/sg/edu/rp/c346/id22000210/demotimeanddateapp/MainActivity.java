5166b74ccc5c85425db88bb259d3e1a11ffdd28f
Wayne <22000210@myrp.edu.sg>
2023-05-11 10:21:12 +0800

    completed halfway

--- app/src/main/java/sg/edu/rp/c346/id22000210/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000210/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,43 @@ package sg.edu.rp.c346.id22000210.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaring the Field Variables
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Linking the Variables to Their Respective UI Elements
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            //It ties a listener object when the button is clicked, and it will then activate the method (action)
+            @Override
+            public void onClick(View v) {
+                //Code for the action of Display Time button
+                btnDisplayTime =
+
+
+            }
+        });
+
     }
 }
